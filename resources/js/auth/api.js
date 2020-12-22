b978133dc392b07c616c60f7ad4a53ab266b28be
William Maddicott <william@Williams-MacBook-Pro.local>
2020-12-22 17:54:08 +0000

    prod build

--- resources/js/auth/api.js
+++ resources/js/auth/api.js
@@ -11,24 +11,24 @@ let axiosConfig = {
 
 export const signUp = user => {
     return axios.post(
-        `${process.env.MIX_URL}/api/auth/signup`,
+        `/api/auth/signup`,
         user,
         axiosConfig
     );
 };
 
 export const tokenActivate = token => {
-    return axios.get(`${process.env.MIX_URL}/api/auth/signup/activate/${token}`);
+    return axios.get(`/api/auth/signup/activate/${token}`);
 };
 
 export const userSignIn = user => {
-    return axios.post(`${process.env.MIX_URL}/api/auth/login`, user);
+    return axios.post(`/api/auth/login`, user);
 };
 
 
 
 export const getUserData = (token) => {
-    return axios.get(`${process.env.MIX_URL}/api/auth/user`, {
+    return axios.get(`/api/auth/user`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
@@ -38,7 +38,7 @@ export const getUserData = (token) => {
 export const logout = () => {
     let token = localStorage.getItem("trailshub:all:userToken");
     localStorage.removeItem("trailshub:all:userToken");
-    return axios.get(`${process.env.MIX_URL}/api/auth/logout`, {
+    return axios.get(`/api/auth/logout`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
@@ -50,7 +50,7 @@ export const submitRoute = (trail) => {
     let token = localStorage.getItem("trailshub:all:userToken");
 console.log('api submit');
     console.log(trail)
-    return axios.post(`${process.env.MIX_URL}/api/auth/trail/submit`,
+    return axios.post(`/api/auth/trail/submit`,
     trail,
          {
         headers: {
@@ -63,7 +63,7 @@ console.log('api submit');
 export const getTrailById = (id) => {
     let token = localStorage.getItem("trailshub:all:userToken");
 
-    return axios.get(`${process.env.MIX_URL}/api/auth/trail/gettrailbyid/${id}`,
+    return axios.get(`/api/auth/trail/gettrailbyid/${id}`,
          {
         headers: {
             Authorization: `Bearer ${token}`
@@ -75,7 +75,12 @@ export const getLocationCoords = (location) =>{
     console.log('ddddfffff');
     console.log(process.env.MIX_URL);
     console.log(process.env.MIX_ROOT);
+    console.log(process.env.MIX_TEST);
+    console.log(process.env.MIX_THINGS);
+    console.log(process.env.MIX_THINGS_AGAIN);
+
+    
     console.log(process.env);
 
-    return axios.get(`${process.env.MIX_URL}/api/location/${location}`)
+    return axios.get(`/api/location/${location}`)
 }
